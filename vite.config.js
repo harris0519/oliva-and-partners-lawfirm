1bebb7680394afd272fcdd4307e899e3364cc0cd
Harris Lazaro <hlazaro@jrs-express.com.ph>
2026-07-20 11:30:08 +0800

    Update website content

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/oliva-and-partner-lawfirm/',
+  base: '/oliva-and-partners-lawfirm/',
 });
